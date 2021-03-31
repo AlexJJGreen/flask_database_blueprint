f745ab1f55167bf459c63ab8fd6605c32f5cc6d4
alexjjgreen <alexjjgreen@gmail.com>
2021-03-31 22:52:27 +0200

    routes models forms db2

--- app/database_2/models.py
+++ app/database_2/models.py
@@ -1,1 +1,8 @@
-from <APP> import <NAME_OF_DB>
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Database_2_DB(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # <-- NB. Autoincrement defaults to true on primary int key cols (SQLAlchemy)
+    comment = db.Column(db.String(144), index=True)
+   
