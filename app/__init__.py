03eb3fe95506983b2f3820a3057b29cad20e1d80
alexjjgreen <alexjjgreen@gmail.com>
2021-04-01 20:20:25 +0200

    database session committing NB: CHECK WHAT DATA YOU PASS

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from .database_2 import database_2
+from app.database_2.models import db as db_2
 
 # create an instance of migrate not bound to app outside of app context
 migrate = Migrate()
@@ -10,9 +12,12 @@ def create_app():
     # create an instance of Flask object and configure from object
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object(Config)
+    db_2.init_app(app)
+    migrate.init_app(app, db_2)
 
     # import blueprints within app context
     with app.app_context():
+        db_2.create_all()
         # import the blueprint <-- REINIT WHEN RUNNING!!!!
         # from database_1 import database_1
         # import the SQLAlchemy instance
@@ -21,21 +26,21 @@ def create_app():
         # app.register_blueprint(database_1, url_prefix="/database_1")
 
         # repeat for all blueprints and databases
-        from app.database_2 import database_2
-        from app.database_2.models import db as database_2_db
+        # from app.database_2 import database_2
+        # from app.database_2.models import db as database_2_db
         app.register_blueprint(database_2, url_prefix="/database_2")
 
         # intialize the databases passing app as arg
         #database_1_db.init_app(app)
-        database_2_db.init_app(app)
+        
 
         # initialize the database migrations for each db instance
         # migrate.init_app(app, database_1_db)
-        migrate.init_app(app, database_2_db)
+        # migrate.init_app(app, db_2)
 
         # create databases
         # database_1_db.create_all()
-        database_2_db.create_all()
+        # database_2_db.create_all()
 
         # import routes at end to avoid circular imports
         from app import routes
--- app/database_2/models.py
+++ app/database_2/models.py
@@ -5,4 +5,7 @@ db = SQLAlchemy()
 class Comment_Model(db.Model):
     id = db.Column(db.Integer, primary_key=True) # <-- NB. Autoincrement defaults to true on primary int key cols (SQLAlchemy)
     comment = db.Column(db.String(144), index=True)
+
+    def __init__(self, comment):
+        self.comment = comment
    
